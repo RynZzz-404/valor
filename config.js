b0478fff624715871b09a31a88c09dbf568e8d97
RynZzz-404 <108714745+RynZzz-404@users.noreply.github.com>
2022-07-09 12:14:16 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -3,11 +3,11 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['919971107409', 'Valor_Bot', true]
+  ['6282252285143', 'Ryan || JB', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
-global.mods = ['919971107409'] // Want some help?
-global.prems = ['919971107409'] // Premium user has unlimited limit
+global.mods = ['6282252285143'] // Want some help?
+global.prems = ['6282252285143'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -28,8 +28,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'Valor Bot'
-global.author = 'Dinesh_Valor🇮🇳'
+global.packname = 'XD-Bot'
+global.author = 'By : Ryan|JB\nowner : 6282252285143'
 
 global.multiplier = 69 // The higher, The harder levelup
 
